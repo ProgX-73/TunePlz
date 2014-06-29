c83146a4169a673115b012cd53d4a1e6014b9cc3
Fred <progx73@gmail.com>
2014-06-29 23:10:00 +0200

    Update tuneplz.js

--- www/tuneplz.js
+++ www/tuneplz.js
@@ -4,8 +4,8 @@ var tuneplz = {
          cordova.exec(
             successCallback, // success callback function
             errorCallback, // error callback function
-            'TunePlz', // mapped to our native Java class called "Calendar"
-            'addCalendarEntry', // with this action name
+            'TunePlug', // mapped to our native Java class called "Calendar"
+            'ACTION_ADD_TUNE_PLZ', // with this action name
             [{                  // and this array of custom arguments to create our entry
                 "title": title,
                 "description": notes,
