75f6ec84d447dce0689e99a8927f4040eae5096f
Fred <progx73@gmail.com>
2014-06-30 00:07:02 +0200

    Update MainRecSoundActivity.java

--- src/com/progx73/tuneplz/MainRecSoundActivity.java
+++ src/com/progx73/tuneplz/MainRecSoundActivity.java
@@ -28,7 +28,7 @@ public class MainRecSoundActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_rec_sound);
+       
 
 
                 alert("In Activity...let"'s go !');
@@ -187,24 +187,6 @@ public class MainRecSoundActivity extends Activity {
         Log.d("info","Peak: "+peakIn+", Frequency: "+frequency+"\n");
         alert("Peak: "+peakIn+", Frequency: "+frequency+"\n");
     }
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main_rec_sound, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
+    
 
 }
